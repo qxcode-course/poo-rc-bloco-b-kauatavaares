e4b5c5bb729722dadbca22523432d4a3e083df75
kauas <kauasoussa12345@gmail.com>
2025-10-22 11:32:15 -0300

    começando motouber

--- myrep/poo/motouber/py/draft.py
+++ myrep/poo/motouber/py/draft.py
@@ -1,8 +1,19 @@
 class Pessoa:
-    def __init__(self, nome: int, dinheiro: str):
-        self.nome = nome
-        self.dinheiro = dinheiro
+    def __init__(self, nome: str, dinheiro: float):
+        self.__nome = nome
+        self.__dinheiro = dinheiro
+
+    def getNome(self):
+        return self.__nome
+    def getDinheiro(self):
+        return self.__dinheiro
 
 
 
 class Moto:
+        def __init__(self,custo: int, passa: Pessoa, motorista: int):
+            self.__custo = custo
+            self.__passa = passa
+            self.__motorista = motorista
+
+        def pagar(self):
